0c9eb86341bf14d0ffbfbb43b3abd0a817b6eb1e
tufmatsu <matsuura.ikuto.005@gmail.com>
2026-03-03 23:04:16 +0900

    loading: add spinning ring animation around icon

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,22 +1,55 @@
 export default function Loading() {
-    return (
+  return (
+    <div style={{
+      position: "fixed",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor: "#fff",
+      zIndex: 9999,
+    }}>
+      <div style={{
+        position: "relative",
+        width: "100px",
+        height: "100px",
+      }}>
+        {/* 回転するリング */}
         <div style={{
-            position: "fixed",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "3px",
-            backgroundColor: "#35c5f0",
-            zIndex: 9999,
-            animation: "loading-bar 2s ease-in-out infinite"
-        }}>
-            <style>{`
-        @keyframes loading-bar {
-          0% { transform: translateX(-100%); }
-          50% { transform: translateX(0); }
-          100% { transform: translateX(100%); }
-        }
-      `}</style>
-        </div>
-    );
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          borderRadius: "50%",
+          border: "3px solid #e0e0e0",
+          borderTopColor: "#35c5f0",
+          animation: "spin 1s linear infinite",
+        }} />
+        {/* 中央のアイコン */}
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src="/hitoheya_lab_icon_final.svg"
+          alt="読み込み中..."
+          width="60"
+          height="60"
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          }}
+        />
+      </div>
+      <style>{`
+                @keyframes spin {
+                    0% { transform: rotate(0deg); }
+                    100% { transform: rotate(360deg); }
+                }
+            `}</style>
+    </div>
+  );
 }
